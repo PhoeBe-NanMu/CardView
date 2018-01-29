bbad19715b3c5267f891561b75c00ca05550a8fe
LeiYang <2443638666@qq.com>
2018-01-29 20:49:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/leiyang/cardview/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.leiyang.cardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CardView cardView;
+    private  SeekBar seekBar1;
+    private SeekBar seekBar2;
+    private SeekBar seekBar3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        cardView = findViewById(R.id.cd1);
+        seekBar1 = findViewById(R.id.sk1);
+        seekBar2 = findViewById(R.id.sk2);
+        seekBar3 = findViewById(R.id.sk3);
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                cardView.setRadius(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                cardView.setCardElevation(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                cardView.setContentPadding(i,i,i,i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
